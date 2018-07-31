fbecbd9b51a455fe96a4592d7a2c54c0e06c2de3
Pascal Filipovicz <pascal.filipovicz@frozenk.net>
2018-07-31 18:14:58 +0200

    fix(crypto): generate a password

--- main.go
+++ main.go
@@ -4,25 +4,72 @@ package main
 // https://en.wikipedia.org/wiki/Scrypt
 
 import (
-	"encoding/base64"
+	"crypto/hmac"
+	"crypto/sha512"
 	"flag"
 	"fmt"
+	"log"
+	"math"
+	"strconv"
 
 	"golang.org/x/crypto/scrypt"
 )
 
-func newPassword(salt, passphrase string, length int) ([]byte, error) {
-	dk, err := scrypt.Key([]byte(passphrase), []byte(salt), 1<<15, 8, 1, length)
+const Base string = "com.filipovicz.vault"
+
+const min = "abcdefghijklmnopqrstuvwxyz"
+const max = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const under = "-_/"
+const number = "0123456789"
+const space = " "
+const special = "!@#$%^&*()=+,.?:;{}[]`~"
+
+func newMasterKey(name, passphrase string) ([]byte, error) {
+	salt := Base + strconv.Itoa(len(name)) + name
+	dk, err := scrypt.Key([]byte(passphrase), []byte(salt), 1<<15, 8, 1, 64)
 	if err != nil {
 		return nil, err
 	}
-
 	return dk, nil
+}
+
+func newSeed(mk []byte, message string) ([]byte, error) {
+	sig := hmac.New(sha512.New, mk)
+	sig.Write([]byte(message))
+	return sig.Sum(nil), nil
+}
+
+func newMessage(service string, length int) string {
+	return Base + strconv.Itoa(len(service)) + service + strconv.Itoa(length)
+}
+
+func newPassword(seed []byte, length int) (string, error) {
+	if length > 64 {
+		return "", fmt.Errorf("length > 64")
+	}
+
+	chars := min + max + number + under + space + special
+	charsLength := len(chars)
+
+	password := ""
+	for index := 0; index < length; index++ {
+		r := seed[index]
+
+		if int(r) < charsLength {
+			c := chars[int(r)]
+			password = password + string(c)
+		} else {
+			m := math.Mod(float64(r), float64(charsLength))
+			password = password + string(chars[int(m)])
+		}
+	}
+
+	return password, nil
 }
 
 func checkFlag(flag string) error {
 	if flag == "" {
-		return fmt.Errorf("is empty")
+		return fmt.Errorf("%s is empty", flag)
 	}
 
 	return nil
@@ -34,13 +81,33 @@ func main() {
 	name := flag.String("name", "", "your name")
 	service := flag.String("service", "", "the name of the service")
 	passphrase := flag.String("passphrase", "", "your passphrase")
-	length := flag.Int("length", 20, "password length")
+	length := flag.Int("length", 35, "password length")
 	flag.Parse()
 
-	fmt.Println(*name)
-	fmt.Println(*service)
-	fmt.Println(*passphrase)
+	if err := checkFlag(*name); err != nil {
+		log.Fatal(err)
+	}
 
-	password, _ := newPassword(*name+*service, *passphrase, *length)
-	fmt.Println(base64.StdEncoding.EncodeToString(password))
+	if err := checkFlag(*service); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := checkFlag(*passphrase); err != nil {
+		log.Fatal(err)
+	}
+
+	mk, err := newMasterKey(*name, *passphrase)
+	if err != nil {
+		log.Fatal(err)
+	}
+	seed, _ := newSeed(mk, newMessage(*service, *length))
+	if err != nil {
+		log.Fatal(err)
+	}
+	password, err := newPassword(seed, *length)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(password)
 }
